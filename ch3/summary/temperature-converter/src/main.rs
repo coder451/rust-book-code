2842e932273cbc75f31552f9f5febf8466986b6c
Gary Price <coder451@gmail.com>
2024-10-19 15:03:39 +1000

    Allow command-line input
    This allows for a single-shot computation.
    It uses much of the same code as the existing
    implementation by constructing the "interactive"
    input from the command line.
    
    It converts the command-line arguments to a
    Vec<String> and processes them in a function,
    keeping the main function small.

--- ch3/summary/temperature-converter/src/main.rs
+++ ch3/summary/temperature-converter/src/main.rs
@@ -1,15 +1,79 @@
 
 use std::io;
+use std::env;
 
 fn main() {
-    loop {
-        if ! ask() { return }
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 1 {
+        loop {
+            if ! ask() { return }
+        }
     }
+    else {
+        let tokens = collect_args(args);
+        compute(&tokens, false);
+    }
+}
+
+fn collect_args(args: Vec<String>) -> String {
+    let arg1 = args.get(1)
+        .expect("Get command line arg at index 1");
+    let mut tokens = String::clone(arg1);
+    if args.len() == 3 {
+        let arg2 = args.get(2)
+            .expect("Can get command ine arg at index 2");
+        tokens += " ";
+        tokens += arg2;
+    }
+    tokens
+}
+
+fn compute(input: &str, verbose: bool)  -> bool {
+    let tokens = tokenize(&input);
+    if ! tokens.0 {
+        if verbose {
+            println!("Did not understand input {input} - try again");
+        }
+        return true
+    }
+
+    let t: f64 = match tokens.1.parse() {
+        Ok(num) => num,
+        Err(_) => {
+            if verbose {
+                println!("Could not understand temperature {input} - try \
+                again.");
+            }
+            return true
+        }
+    };
+
+    let (unit, other_unit) = match tokens.2 {
+        "c"=> ("C", "F"),
+        "C"=> ("C", "F"),
+        "f"=> ("F", "C"),
+        "F"=> ("F", "C"),
+        _ => {
+            if verbose {
+                println!("Did not understand the unit in {input}");
+            }
+            return false
+        }
+    };
+
+    if verbose {
+        println!("Result: {0:.1} {1}", t_c(t, unit), other_unit);
+    }
+    else {
+        println!("{0:.1}{1}", t_c(t, unit), other_unit);
+    }
+    true
+
 }
 
 fn ask() -> bool {
     println!("Enter the temperature as value type where value is a \
-    decimal and type is F or C. Input q to quit");
+        decimal and type is F or C. Input q to quit");
 
     let mut t = String::new();
         
@@ -25,36 +89,7 @@ fn ask() -> bool {
         return false
     };
 
-    let tokens = tokenize(&input);
-    if ! tokens.0 {
-        println!("Did not understand input {input} - try again");
-        return true
-    }
-
-    let t: f64 = match tokens.1.parse() {
-        Ok(num) => num,
-        Err(_) => {
-            println!("Could not understand temperature {input} - try \
-            again.");
-            return true
-        }
-    };
-
-    let (unit, other_unit) = match tokens.2 {
-        "c"=> ("C", "F"),
-        "C"=> ("C", "F"),
-        "f"=> ("F", "C"),
-        "F"=> ("F", "C"),
-        _ => {
-            println!("Did not understand the unit in {input}");
-            return false
-        }
-    };
-
-    println!("Result: {0:.1} {1}", t_c(t, unit), other_unit);
-
-    true
-
+    compute(input, true)
 }
 
 fn tokenize(t: &str) -> (bool, &str, &str) {
@@ -73,7 +108,11 @@ fn tokenize(t: &str) -> (bool, &str, &str) {
         if len < 2 {
             return (false, t, t)
         }
-        (true, &tok[..(len - 1)], &tok[(len - 1)..len])
+        (
+            true, 
+            &tok[..(len - 1)], 
+            &tok[(len - 1)..len]
+        )
     }
     else {
         (false, t, t)
@@ -84,7 +123,10 @@ fn t_c(t: f64, unit: &str) -> f64 {
     if unit == "C" {
         (t / 5.0) * 9.0 + 32.0
     }
-    else {
+    else if unit == "F" {
         (t - 32.0) * 5.0 / 9.0
+    }
+    else {
+        panic!("Unexpected unit");
     }
 }
