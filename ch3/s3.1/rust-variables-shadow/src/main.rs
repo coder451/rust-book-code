28ab14558251837d25fff1f2ddccb5fde9db5d74
Gary Price <coder451@gmail.com>
2024-10-19 10:45:40 +1000

    Print a const value

--- ch3/s3.1/rust-variables-shadow/src/main.rs
+++ ch3/s3.1/rust-variables-shadow/src/main.rs
@@ -15,4 +15,8 @@ fn main() {
     let spaces = spaces.len();
     println!("Now the value of spaces is {spaces}");
 
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    println!("The constant THREE_HOURS_IN_SECONDS is {THREE_HOURS_IN_SECONDS}");
+
+
 }
