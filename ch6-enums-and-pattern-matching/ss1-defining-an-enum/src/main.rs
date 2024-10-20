95cfdc8730c461c906ee2208f813e52f69348a72
Gary Price <coder451@gmail.com>
2024-10-21 08:17:23 +1000

    Make the Animal enum more complicated.
    The variant constructors can be plain or named.
    When named, the variant is said to be struct-like.
    Struct-like variants use {} in their constructor.

--- ch6-enums-and-pattern-matching/ss1-defining-an-enum/src/main.rs
+++ ch6-enums-and-pattern-matching/ss1-defining-an-enum/src/main.rs
@@ -16,10 +16,13 @@ enum Message {
     Write(String),
     ChangeColor(i32, i32, i32),
 }
-
+// In this enum, Dog is an enum variant, while Cat is 
+// a "struct-like enum variant" because it has named constructor
+// parameters. It looks like a struct, and uses {} in its 
+// constructor.
 enum Animal {
-    Dog,
-    Cat,
+    Dog(String, f64),
+    Cat { name: String, weight: f64 },
 }
 
 fn main() {
@@ -37,7 +40,12 @@ fn main() {
     let home = IpAddr::V4(127, 0, 0, 1);
 
     let loopback = IpAddr::V6(String::from("::1"));
-        
-    let mut a: Animal = Animal::Dog;
-    a = Animal::Cat;
+
+    // This constructs a mutable reference to a Dog
+    let mut a = Animal::Dog("Cocoa".to_string(), 37.2);
+    // This changes the Animal to be a Cat
+    a = Animal::Cat { name: "Spotty".to_string(), weight: 2.7 };
+    // This makes another cat, using the opposite order of named parameters
+    let b = Animal::Cat { weight: 2.7, name: "Spotty".to_string() };
+
 }
