f61563fd295d82d28dbc6e4e9b25f6e04bb006c0
Gary Price <coder451@gmail.com>
2024-10-21 07:37:00 +1000

    Add an impl block

--- ch5/s5.3/multiple-impl-blocks/src/main.rs
+++ ch5/s5.3/multiple-impl-blocks/src/main.rs
@@ -5,6 +5,12 @@ struct Rectangle {
 }
 
 impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
+impl Rectangle {
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
     }
@@ -24,6 +30,7 @@ fn main() {
         height: 45,
     };
 
+    println!("Area of rect1: {}", rect1.area());
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
