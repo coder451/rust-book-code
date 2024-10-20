2926c7155196d9f08cff689a6b22806d6b0ee8e6
Gary Price <coder451@gmail.com>
2024-10-21 07:55:54 +1000

    Add a function taking an IP address
    Use the variants either directly or thoruh an
    instance.

--- ch6-enums-and-pattern-matching/ss1-defining-an-enum/src/main.rs
+++ ch6-enums-and-pattern-matching/ss1-defining-an-enum/src/main.rs
@@ -3,10 +3,17 @@ enum IpAddrKind {
     V6,
 }
 
+fn route(ip_kind: IpAddrKind) {}
+
 fn main() {
     println!("Enums and pattern matching");
 
     let four = IpAddrKind::V4;
     let six = IpAddrKind::V6;
 
+    route(IpAddrKind::V4);
+    route(IpAddrKind::V6);
+
+    route(four);
+    route(six);
 }
