2f28f4d6192ef9bcd08a7155ae53497637267d09
Gary Price <coder451@gmail.com>
2024-10-21 07:31:11 +1000

    Adding associated function to create a square

--- ch5/s5.3/associated-functions/src/main.rs
+++ ch5/s5.3/associated-functions/src/main.rs
@@ -4,26 +4,18 @@ struct Rectangle {
     height: u32,
 }
 
+// Associated function - factory making a square
 impl Rectangle {
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
     }
 }
 
 fn main() {
-    let rect1 = Rectangle {
-        width: 30,
-        height: 50,
-    };
-    let rect2 = Rectangle {
-        width: 10,
-        height: 40,
-    };
-    let rect3 = Rectangle {
-        width: 60,
-        height: 45,
-    };
+    let rect = Rectangle::square(30);
 
-    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
-    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+    println!("rect: {rect:?}");
 }
