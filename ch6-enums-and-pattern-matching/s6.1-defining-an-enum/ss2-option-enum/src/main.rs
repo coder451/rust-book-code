38e12d91c04567c414f3128749ce84d986de0932
Gary Price <coder451@gmail.com>
2024-10-21 09:19:01 +1000

    Display Option enum example

---
+++ ch6-enums-and-pattern-matching/s6.1-defining-an-enum/ss2-option-enum/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    println!("The Option enum");
+
+    let some_number = Some(5);
+    let some_char = Some('e');
+
+    let absent_number: Option<i32> = None;
+
+    println!("some_number: {:?}", some_number);
+    println!("some_char: {:?}", some_char);
+    println!("absent_number: {:?}", absent_number);
+
+}
