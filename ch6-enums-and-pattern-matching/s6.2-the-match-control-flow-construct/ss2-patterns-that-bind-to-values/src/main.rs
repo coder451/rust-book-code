a22630f6b6f6e35a0c7c5159536c53b92a99f1b7
Gary Price <coder451@gmail.com>
2024-10-21 13:28:10 +1000

    Matching an internal enum

--- ch6-enums-and-pattern-matching/s6.2-the-match-control-flow-construct/ss2-patterns-that-bind-to-values/src/main.rs
+++ ch6-enums-and-pattern-matching/s6.2-the-match-control-flow-construct/ss2-patterns-that-bind-to-values/src/main.rs
@@ -4,7 +4,8 @@ enum UsState {
     Alaska,
     // --snip--
 }
-
+// Quarters are related to states because each state has a quarter
+// with a characteristic scene on it
 enum Coin {
     Penny,
     Nickel,
@@ -12,6 +13,22 @@ enum Coin {
     Quarter(UsState),
 }
 
+// In addition to the value, this function prints the corresponding 
+// state
+fn value_in_cents(coin: &Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {state:?}!");
+            25
+        }
+    }
+}
+
 fn main() {
     println!("Patterns that bind to values");
+
+    println!("{}", value_in_cents(&Coin::Quarter(UsState::Alabama)));
 }
