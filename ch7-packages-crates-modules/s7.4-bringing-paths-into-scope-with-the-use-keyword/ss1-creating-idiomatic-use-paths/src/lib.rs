af1103bd79e9e0b0f750d14fbf613f1b02b8ec60
Gary Price <coder451@gmail.com>
2024-10-24 08:22:36 +1000

    Copy from previous and update

---
+++ ch7-packages-crates-modules/s7.4-bringing-paths-into-scope-with-the-use-keyword/ss1-creating-idiomatic-use-paths/src/lib.rs
@@ -0,0 +1,74 @@
+fn deliver_order() {}
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order(); // <-- super
+    }
+
+    fn cook_order() {}
+
+    pub struct Breakfast {        // Public struct
+        pub toast: String,        // public member
+        seasonal_fruit: String,   // private member
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    // A public enum
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+}
+
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+
+        fn serve_order() {}
+
+        fn take_payment() {}
+    }
+}
+
+
+mod customer {
+    // Bring hosting module into this scope
+    use crate::front_of_house::hosting;
+    
+    pub fn eat_at_restaurant() {
+
+        // Access a hosting function
+        // The hosting module is public and so is the add_to_waitlist 
+        // function
+        hosting::add_to_waitlist();
+
+        // Order a breakfast in the summer with Rye toast
+        let mut meal = back_of_house::Breakfast::summer("Rye");
+        // Change our mind about what bread we'd like
+        meal.toast = String::from("Wheat");
+        println!("I'd like {} toast please", meal.toast);
+
+        // The next line won't compile if we uncomment it; we're not allowed
+        // to see or modify the seasonal fruit that comes with the meal
+        //meal.seasonal_fruit = String::from("blueberries");
+
+        // Variants of a public enum are public
+        let order1 = back_of_house::Appetizer::Soup;
+        let order2 = back_of_house::Appetizer::Salad;
+    }
+}
