0835ad7e9ad54239d4ab0867e75aa1912179c7cc
Gary Price <coder451@gmail.com>
2024-10-24 07:23:32 +1000

    Making Structs and Enums public
    This commit includes an erroneous line, where
    a private struct member is accessed.

---
+++ ch7-packages-crates-modules/s7.3-paths-for-module-tree-items/ss2-making-structs-and-enums-public/src/lib.rs
@@ -0,0 +1,52 @@
+fn deliver_order() {}
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order(); // <-- super
+    }
+
+    fn cook_order() {}
+
+    pub struct Breakfast {        // Public struct
+        pub toast: String,        // public member
+        seasonal_fruit: String,   // private member
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+}
+
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+
+        fn serve_order() {}
+
+        fn take_payment() {}
+    }
+}
+
+pub fn eat_at_restaurant() {
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // The next line won't compile if we uncomment it; we're not allowed
+    // to see or modify the seasonal fruit that comes with the meal
+    meal.seasonal_fruit = String::from("blueberries");
+}
