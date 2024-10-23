27ded757669d17dd918dc111484ea782a0ac58f4
Gary Price <coder451@gmail.com>
2024-10-24 08:45:56 +1000

    Add a stand-alone example

--- ch7-packages-crates-modules/s7.4-bringing-paths-into-scope-with-the-use-keyword/ss2-creating-new-names-with-the-as-keyword/src/main.rs
+++ ch7-packages-crates-modules/s7.4-bringing-paths-into-scope-with-the-use-keyword/ss2-creating-new-names-with-the-as-keyword/src/main.rs
@@ -1,3 +1,26 @@
+pub mod MyIo {
+    pub struct Result {
+        pub n: i32
+    }
+}
+pub mod YourIo {
+    pub struct Result {
+        pub n: i32
+    }
+}
+
+use MyIo::Result;
+use YourIo::Result as YourIoResult;
+
+
+fn function1() -> Result {
+    Result{n: 0}
+}
+
+fn function2() -> YourIoResult {
+    YourIoResult{n: 0}
+}
+
 fn main() {
     println!("Hello, world!");
 }
