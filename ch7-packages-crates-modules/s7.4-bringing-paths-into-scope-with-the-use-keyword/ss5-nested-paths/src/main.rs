22505788ae5bc63c2e50f5f15e50e2ae1c6ce0e1
Gary Price <coder451@gmail.com>
2024-10-26 07:02:37 +1000

    Example with two imports from std

---
+++ ch7-packages-crates-modules/s7.4-bringing-paths-into-scope-with-the-use-keyword/ss5-nested-paths/src/main.rs
@@ -0,0 +1,30 @@
+use rand::Rng;
+
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("Using Nested Paths to Clean Up Large Use Lists");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {guess}");
+
+    let guess: i32 = guess.trim().parse().expect("Failed to parse guess");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
+}
