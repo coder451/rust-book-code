24a91193b7c72ff38975d87a61cc4e653203839a
Gary Price <coder451@gmail.com>
2024-10-27 11:40:49 +1000

    Move top-level module into a file

--- ch7-packages-crates-modules/s7.5-modules-in-separate-files/src/lib.rs
+++ ch7-packages-crates-modules/s7.5-modules-in-separate-files/src/lib.rs
@@ -40,21 +40,22 @@ pub mod back_of_house {
     }
 }
 
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
+mod front_of_house;
+// mod front_of_house {
+//     pub mod hosting {
+//         pub fn add_to_waitlist() {}
 
-        pub fn seat_at_table() {}
-    }
+//         pub fn seat_at_table() {}
+//     }
 
-    pub mod serving {
-        pub fn take_order() {}
+//     pub mod serving {
+//         pub fn take_order() {}
 
-        pub fn serve_order() {}
+//         pub fn serve_order() {}
 
-        pub fn take_payment() {}
-    }
-}
+//         pub fn take_payment() {}
+//     }
+// }
 
 pub mod customer {
     // Bring hosting module into this scope
