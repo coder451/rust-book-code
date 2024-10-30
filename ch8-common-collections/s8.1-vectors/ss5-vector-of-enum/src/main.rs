7f92eeae14fdc0461ae02abd12f1bfd218c4e0ce
Gary Price <coder451@gmail.com>
2024-10-30 10:02:53 +1000

    Fix warnings

--- ch8-common-collections/s8.1-vectors/ss5-vector-of-enum/src/main.rs
+++ ch8-common-collections/s8.1-vectors/ss5-vector-of-enum/src/main.rs
@@ -3,7 +3,6 @@ fn main() {
     // Otherwise, see traits (Ch 17.)
     println!("Using an enum to store multiple types in a vector");
     
-    #[derive(Debug)]
     enum SpreadsheetCell {
         Int(i32),
         Float(f64),
@@ -16,7 +15,22 @@ fn main() {
         SpreadsheetCell::Float(10.12),
     ];
 
-    println!("{:?}", &row[0]);
-    println!("{:?}", &row[1]);
-    println!("{:?}", &row[2]);
+    // This illustrates explicitly accessing the individual values from 
+    // the enum
+    let i = match row[0] {
+        SpreadsheetCell::Int(i) => i,
+        _ => -1
+    };
+    let f = match row[2] {
+        SpreadsheetCell::Float(f) => f,
+        _ => -1.0
+    };
+    let t = match &row[1] {
+        SpreadsheetCell::Text(t) => String::from(t),
+        _ => String::from("")
+    };
+
+    println!("{:?}", i);
+    println!("{:?}", f);
+    println!("{:?}", &t);
 }
