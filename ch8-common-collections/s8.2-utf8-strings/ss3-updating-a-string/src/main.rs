fb40481ad3a31dc7c3d4924be35b980469577df3
Gary Price <coder451@gmail.com>
2024-11-01 08:58:15 +1000

    push_str uses a reference

--- ch8-common-collections/s8.2-utf8-strings/ss3-updating-a-string/src/main.rs
+++ ch8-common-collections/s8.2-utf8-strings/ss3-updating-a-string/src/main.rs
@@ -1,10 +1,31 @@
 fn main() {
     println!("Ch 8.2-3 Updating a String");
 
-    // Add to the end of a String
-    let mut s = String::from("foo");
-    println!("Starting string: {}", &s);
-    s.push_str("bar");
-    println!("After push_str(\"bar\"): {}\n", &s);
+    {
+        // Add to the end of a String
+        let mut s = String::from("foo");
+        println!("Starting string: {}", &s);
+        s.push_str("bar");
+        println!("After push_str(\"bar\"): {}\n", &s);
+    }
+    {
+        // push_str uses a reference &str
+        println!("push_str uses a reference");
+        let mut s1 = String::from("foo");
+        println!("Starting with {}", &s1);
+        let s2 = "bar";
+        println!("Append s2: {}", &s2);
+        s1.push_str(s2);
+        println!("Now the string has been augmented: {}", &s1);
+        println!("What we appended is still there: s2 is {s2}\n");    
+    }
+
+    {
+        println!("The push method takes a single character");
+        let mut s = String::from("LO");
+        println!("Start with s = \"{}\"", &s);
+        s.push('L');
+        println!("After adding one character with push: \"{}\"", s);
+    }
 
 }
