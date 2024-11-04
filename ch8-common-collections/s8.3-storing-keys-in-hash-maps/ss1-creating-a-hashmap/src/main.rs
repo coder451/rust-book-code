26ef699ff0aa33194a21d9df2c254d532000747d
Gary Price <coder451@gmail.com>
2024-11-04 10:46:36 +1000

    Add notes

--- ch8-common-collections/s8.3-storing-keys-in-hash-maps/ss1-creating-a-hashmap/src/main.rs
+++ ch8-common-collections/s8.3-storing-keys-in-hash-maps/ss1-creating-a-hashmap/src/main.rs
@@ -1,11 +1,17 @@
 fn main() {
     println!("Ch8.3-1 Creating a HashMap");
+
+    // Convention is to "use" the struct name and so not need
+    // qualification i the code
     use std::collections::HashMap;
 
+    // The types used in the map are inferred from its use later in 
+    // the file.
     let mut scores = HashMap::new();
 
     scores.insert(String::from("Blue"), 10);
     scores.insert(String::from("Yellow"), 50);
 
+    // The default output prints the keys second
     println!("{:?}", scores);
 }
